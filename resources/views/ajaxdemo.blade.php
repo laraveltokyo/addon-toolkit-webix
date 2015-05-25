5995f2e2a4d626ee0c43d15fa6bec32fb56d04bc
Fumio Furukawa <jumilla@laravel.tokyo>
2015-05-25 16:22:59 +0900

    Implement ajax demo.

--- resources/views/ajaxdemo.blade.php
+++ resources/views/ajaxdemo.blade.php
@@ -1,5 +1,47 @@
-@extends($__addon_name . '::' . layout)
+@extends($__addon_name . '::' . 'layout')
 
 @section('content')
-<h1>{{ addon_trans($__addon_name, 'messages.sample_title') }}</h1>
+
+<script type="text/javascript" charset="utf-8">
+	'use strict'
+
+	webix.ui({
+		cols: [
+			{
+				view:   'button',
+				label:  'Success',
+				height: 50,
+				click:  function () {
+					callAjax('success')
+				},
+			},
+			{
+				view:   'button',
+				label:  '500 Error',
+				height: 50,
+				click:  function () {
+					callAjax('error')
+				},
+			},
+		],
+	})
+
+	function callAjax(action) {
+		webix.ajax().post(
+			'ajax/' + action,
+			{
+				_token: '{!! csrf_token() !!}',
+			}
+		)
+		.then(function (data) {
+			console.log(data)
+			alert('Success: ' + data.text())
+		})
+		.fail(function (error) {
+			console.log(error)
+			alert('Error: ' + error.status + ':' + error.statusText)
+		})
+	}
+
+</script>
 @stop
